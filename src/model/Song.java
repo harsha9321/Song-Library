4c0dfe028d45a72b4d5dba5ceeecdf2ab93c64e3
Sreeharsha Dandamudi <sreeharsha.dandamudi@rutgers.edu>
2017-09-28 15:58:03 -0400

    Song class with song info and basic view

---
+++ src/model/Song.java
@@ -0,0 +1,68 @@
+package application;
+
+public class Song {
+	
+	private String title;
+	private String artist;
+	private String album;
+	private int year;
+	
+	public Song(String t, String art, String alb, int y) {
+		this.title = t;
+		this.artist = art;
+		this.album = alb;
+		this.year = y;
+	}
+	
+	public Song(String t, String art) {
+		this(t, art, "", -1);
+	}
+	
+	public Song(String t, String art, String alb) {
+		this(t, art, alb, -1);
+	}
+	
+	public Song(String t, String art, int y) {
+		this(t, art, "", y);
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public String getArtist() {
+		return this.artist;
+	}
+	
+	public String getAlbum() {
+		return this.album;
+	}
+	
+	public int getYear() {
+		return this.year;
+	}
+	
+	public void setTitle(String t) {
+		this.title = t;
+	}
+	
+	public void setArtist(String art) {
+		this.artist = art;
+	}
+	
+	public void setAlbum(String alb) {
+		this.album = alb;
+	}
+	
+	public void setYear(int y) {
+		this.year = y;
+	}
+	
+	public String toString() {
+		String s = getTitle() + "=" +
+					getArtist() + "=" +
+					getAlbum() + "=" +
+					getYear();
+		return s;
+	}
+}
