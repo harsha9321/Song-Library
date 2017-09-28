9e385cb07209efbf842c28ed5269fd48607c1c78
Akash <akashdesai10@gmail.com>
2017-09-28 16:16:29 -0400

    added the string library

---
+++ src/model/songLibrary.java
@@ -0,0 +1,47 @@
+package model;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.io.*;
+import java.text.Collator;
+import java.util.*;
+import javafx.collections.ObservableList;
+import java.text.Collator;
+
+public class songLibrary {
+	//String file_name = "Songlibrary.txt";
+		songLibrary model;
+		static LinkedList<Song> songList;
+	    ObservableList<Song> obList;
+
+		
+		public songLibrary(){
+			songList = new LinkedList<Song>();  
+			obList =  FXCollections.observableArrayList();
+			//read the txt file whenever a songLibrary is creates
+		}
+		//remove static
+		public static int add(String name, String artist, String album, String year){
+			Song newly_added_song = new Song(name, artist, album, year);
+			songList.add(newly_added_song);	
+			return 0;
+			
+			
+		}
+		
+		public static void main(String args[]){
+			
+			songLibrary.add("dope", "Akash", "asd", "asd");
+			Song temp;
+			Iterator<Song> itr= songList.iterator();  
+			  while(itr.hasNext()){  
+			 temp = itr.next();
+			   System.out.println(temp.toString()); 
+		   }  
+			  
+			
+		}
+
+}
